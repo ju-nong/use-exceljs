5f9021e5dbcad742b4ccfa6cb1ab7a5d9c754fcb
이준용 <iiiuooyoo@gmail.com>
2024-04-16 16:59:19 +0900

    feat : 엑셀 이름 및 Sheet 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,62 @@
-function createURL(): string {
-    return window.URL.createObjectURL(
-        new Blob(
-            ["Hello World"],
+import { useRef, useMemo } from "react";
+import { Workbook } from "exceljs";
 
-            {
-                type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            },
-        ),
+// Workbook 생성 및 반환
+function createWorkbook(startDate: string, endDate: string): Workbook {
+    const wb = new Workbook();
+    const sheet = wb.addWorksheet(`논술평가사용통계_${startDate}_${endDate}`);
+
+    return wb;
+}
+
+// 엑셀 파일 정보 생성 및 반환
+async function createURL(workbook: Workbook): Promise<string> {
+    const data = await workbook.xlsx.writeBuffer();
+
+    return window.URL.createObjectURL(
+        new Blob([data], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        }),
     );
 }
 
-function createLink(url: string): HTMLAnchorElement {
+function createLink(
+    url: string,
+    startDate: string,
+    endDate: string,
+): HTMLAnchorElement {
     const link = document.createElement("a");
     link.href = url;
-    link.download = "Exam";
+    link.download = `논술평가사용통계_${startDate}_${endDate}`;
 
     return link;
 }
 
 function App() {
-    function handleDownload() {
-        const url = createURL();
-        const link = createLink(url);
+    const $startDate = useRef<HTMLInputElement>(null);
+    const $endDate = useRef<HTMLInputElement>(null);
+
+    async function handleDownload() {
+        if ($startDate.current === null || $endDate.current === null) {
+            return;
+        }
+
+        const { value: startDate } = $startDate.current;
+        const { value: endDate } = $endDate.current;
+
+        if (startDate.length < 1 || endDate.length < 1) {
+            alert("날짜를 입력해주세요.");
+            return;
+        }
+
+        // Workbook 생성
+        const wb = createWorkbook(startDate, endDate);
+
+        // Excel Object URL 생성
+        const url = await createURL(wb);
+
+        // Download Link 생성
+        const link = createLink(url, startDate, endDate);
 
         document.body.appendChild(link);
         link.click();
@@ -32,6 +67,12 @@ function App() {
 
     return (
         <div>
+            Start date
+            <input type="date" ref={$startDate} /> <br />
+            <br />
+            End date
+            <input type="date" ref={$endDate} /> <br />
+            <br />
             <button onClick={handleDownload}>엑셀 다운로드</button>
         </div>
     );
